daf11eb013b3fbb71612cb2e2dc2613cb42c2056
Ashif Shaikh <ashifshaikh9a@gmail.com>
2026-08-15 21:18:08 +0530

    refactor: main layout updated

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -4,13 +4,20 @@ import Footer from "./footer"
 
 const MainLayout = () => {
   return (
-    <>
-      <Navbar />
-      <main className="container mx-auto overflow-hidden">
-        <Outlet />
-      </main>
-      <Footer />
-    </>
+    <div className="relative min-h-screen overflow-hidden bg-background">
+      {/* Content */}
+      <div className="relative z-10">
+        <Navbar />
+
+        <main className="w-full">
+          <div className="mx-auto w-full max-w-7xl px-6">
+            <Outlet />
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    </div>
   )
 }
 
